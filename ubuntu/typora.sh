27ae920902630a52ad26c2535c46cb9aa91bcfd2
Taylor <zeitue@gmail.com>
2020-09-18 02:23:53 -0500

    Added Typora

---
+++ ubuntu/typora.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Typora
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt update
+sudo apt install -y typora
