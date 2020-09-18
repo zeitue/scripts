4af6412f6c192e457bf945bcfeb62389651345e2
Taylor <zeitue@gmail.com>
2020-09-17 20:42:06 -0500

    Added Visual Studio Code

---
+++ ubuntu/visual-studio-code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Visual Studio Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+rm packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code
