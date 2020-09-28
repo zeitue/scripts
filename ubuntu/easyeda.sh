94f6420a5797fdc5195de46c14becb38940007bf
Taylor Ramirez <tramirez@pcdworks.com>
2020-09-28 11:50:17 -0500

    Fixed directory issue in easyeda.sh

--- ubuntu/easyeda.sh
+++ ubuntu/easyeda.sh
@@ -4,7 +4,9 @@
 VERSION=6.4.7
 wget -c "https://image.easyeda.com/files/easyeda-linux-x64-$VERSION.zip"
 unzip "easyeda-linux-x64-$VERSION.zip" -d easyeda
-chmod +x ./easyeda/install.sh
-sudo ./easyeda/install.sh
+cd easyeda
+chmod +x ./install.sh
+sudo ./install.sh
+cd ..
 rm -Rf easyeda
 rm "easyeda-linux-x64-$VERSION.zip"
