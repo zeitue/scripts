2e99851b03ef34b146297531b0a4f248f29d60d6
Taylor <zeitue@gmail.com>
2020-09-30 14:40:06 -0500

    Added TouLite

---
+++ ubuntu/touplite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ToupLite
+wget -c http://www.touptek.com//upload/download/ToupTekToupLite.x64.tar.bz2
+tar -xf ToupTekToupLite.x64.tar.bz2
+sudo ./ToupTekToupLite.x64.sh
+sudo mv /root/Desktop/ToupLite.desktop /usr/local/share/applications/
+sudo rm ./ToupTekToupLite.x64.sh ./ToupTekToupLite.x64.tar.bz2
