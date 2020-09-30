cf37b6c63116bef0de2c8b07afc00352c8c887ca
Taylor <zeitue@gmail.com>
2020-09-30 16:36:48 -0500

    Added Processing

---
+++ ubuntu/processing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Processing
+VERSION=3.5.4
+wget "https://download.processing.org/processing-${VERSION}-linux64.tgz"
+tar -xf "processing-${VERSION}-linux64.tgz"
+sudo mv "processing-${VERSION}" /opt/processing
+sudo sh /opt/processing/install.sh
+sudo ln -s /opt/processing/processing /usr/local/bin/
+rm "processing-${VERSION}-linux64.tgz"
