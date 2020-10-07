6d1816a7cfb8a4b91311cec647c380e04b1351dc
Taylor <zeitue@gmail.com>
2020-10-07 00:39:05 -0500

    Added bash

---
+++ ubuntu/bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install bash
+sudo apt install -y bash
+
+# Install Bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --silent
+sed -i -e 's/bobby/pure/g' ~/.bashrc
+sed -i -e 's/BASH_IT=.*$/BASH_IT=\"$HOME\/.bash_it\"/g' ~/.bashrc
+sudo cp -R ~/.bash_it /etc/skel/
+sudo cp ~/.bashrc /etc/skel/
+
