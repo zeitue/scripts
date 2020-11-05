5b93f0c0b10c693c5ff6933b4113d93cc40fba47
Taylor Ramirez <tramirez@pcdworks.com>
2020-11-05 15:07:28 -0600

    Split up install commands in file-manager-extras.sh to avoid dependency error

--- ubuntu/file-manager-extras.sh
+++ ubuntu/file-manager-extras.sh
@@ -3,22 +3,24 @@
 # Caja extensions
 if [ -e /usr/bin/caja ]; then
     sudo apt install -y caja-open-terminal caja-rename caja-sendto\
-    caja-mediainfo caja-image-converter caja-eiciel caja-gtkhash\
-    caja-xattr-tags caja-nextcloud
+      caja-mediainfo caja-image-converter caja-eiciel caja-gtkhash\
+      caja-xattr-tags caja-nextcloud
 fi
 
 # Nemo extensions
 if [ -e /usr/bin/nemo ]; then
-    sudo apt install -y nemo-audio-tab nemo-emblems nemo-gtkhash\
-    nemo-image-converter nemo-python nemo-nextcloud
+    sudo apt install -y nemo-audio-tab
+    sudo apt install -y nemo-emblems
+    sudo apt install -y nemo-gtkhash nemo-image-converter\
+      nemo-python nemo-nextcloud
 fi
 
 
 # Nautilus extensions
 if [ -e /usr/bin/nautilus ]; then
     sudo apt install -y rabbitvcs-nautilus nautilus-nextcloud\
-    nautilus-extension-gnome-terminal\
-    nautilus-image-converter nautilus-sendto nautilus-gtkhash
+      nautilus-extension-gnome-terminal\
+      nautilus-image-converter nautilus-sendto nautilus-gtkhash
 fi
 
 # Thumbnailers
