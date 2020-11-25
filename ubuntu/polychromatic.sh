ebe7907559279e23de1d48a8e459053c682390c5
Taylor <zeitue@gmail.com>
2020-11-24 19:19:45 -0600

    Added polychromatic

---
+++ ubuntu/polychromatic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# polychromatic
+
+## Note install OpenRazer first
+
+echo "Installing polychromatic"
+
+sudo add-apt-repository -y ppa:polychromatic/stable
+sudo apt update
+sudo apt install -y polychromatic
+
