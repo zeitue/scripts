78b8fdbf34c51aa920edfd13f49994bb924582fc
Taylor <zeitue@gmail.com>
2022-01-19 21:50:56 -0600

    Relocated files

--- ubuntu/setup/libreoffice.sh
+++ ubuntu/setup/libreoffice.sh
@@ -2,7 +2,9 @@
 
 # LibreOffice
 echo "Installing LibreOffice"
+
 sudo add-apt-repository -y "ppa:libreoffice/ppa"
 sudo apt update
 sudo apt install -y libreoffice
 sudo apt upgrade -y libreoffice
+
