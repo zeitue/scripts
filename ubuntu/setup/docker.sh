1d713288d96bd5fb17f15f5121c5a62f57a7acc3
Taylor <zeitue@gmail.com>
2022-01-19 23:35:40 -0600

    Updated docker.sh

--- ubuntu/setup/docker.sh
+++ ubuntu/setup/docker.sh
@@ -4,11 +4,11 @@
 
 ## Note: only LTS releases are supported
 echo "Installing Docker"
-RELEASE=$(awk -F= '$1 == "UBUNTU_CODENAME" {gsub(/"/, "", $2); print $2}' /etc/os-release)
+RELEASE=$(lsb_release -cs)
 
 # Install dependencies
 sudo apt-get install -y apt-transport-https ca-certificates \
-                        gnupg-agent software-properties-common
+                        gnupg-agent software-properties-common curl
 
 # Add repository
 wget -qO - "https://download.docker.com/linux/ubuntu/gpg" |\
@@ -22,11 +22,6 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $RELEASE stable"
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
-# Add users to group
-if groups $USER | grep -q '\bdomain users\b'; then
-    for u in $(wbinfo -u); do
-        sudo usermod -a -G docker $u;
-    done
-else
-    sudo usermod -a -G docker $USER
-fi
+# Add user to group
+sudo usermod -a -G docker $USER
+
--- ubuntu/setup/openssh.sh
+++ ubuntu/setup/openssh.sh
@@ -8,9 +8,9 @@ echo "Installing SSH"
 sudo apt install -y openssh-server openssh-sftp-server sshfs
 
 # Enable sshd
-sudo systemctl enable ssh
-sudo systemctl start ssh
+sudo systemctl enable --now ssh
 sudo ufw allow OpenSSH
 
 # Allow all users to mount FUSE file systems
 sudo sed -i '/user_allow_other/s/^#//g' /etc/fuse.conf
+
