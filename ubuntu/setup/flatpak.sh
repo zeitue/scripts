9d9129b1b279e1f6abbdcaf7a51261c6a09d68f3
Taylor <zeitue@gmail.com>
2022-01-27 00:46:56 -0600

    Added package manager plugins for flatpak

--- ubuntu/setup/flatpak.sh
+++ ubuntu/setup/flatpak.sh
@@ -3,5 +3,14 @@
 # setup flatpak
 echo "Installing Flatpak"
 sudo apt install -y flatpak flatpak-xdg-utils
+
+if [[ -f "/usr/bin/plasma-discover" ]]; then
+sudo apt install -y plasma-discover-backend-flatpak
+fi
+
+if [[ -f "/usr/bin/gnome-software" ]]; then
+sudo apt install -y gnome-software-plugin-flatpak
+fi
+
 echo "Adding Flathub repository"
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
