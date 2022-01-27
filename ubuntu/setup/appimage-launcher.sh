3d0fb6846e45bf177620304563e5a60521d83fee
Taylor <zeitue@gmail.com>
2022-01-27 17:21:55 -0600

    Added appimage-launcher.sh

---
+++ ubuntu/setup/appimage-launcher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+getLatest() {
+  REPO=$1
+  SUFFIX=$2
+  curl --silent https://api.github.com/repos/$REPO/releases/latest | \
+    python3 -c "
+import sys;
+from json import loads as l;
+x = l(sys.stdin.read());
+print(''.join(s['browser_download_url']
+for s in x['assets'] if s['name'].endswith('.$SUFFIX')))"
+}
+
+echo "Installing AppImageLauncher"
+
+URL=$(getLatest TheAssassin/AppImageLauncher bionic_amd64.deb)
+FILE=$(basename "$URL")
+wget -c "$URL"
+sudo apt install -y ./$FILE
+rm ./$FILE
+
