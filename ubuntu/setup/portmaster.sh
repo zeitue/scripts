81863798ff462fe7208bbf9278047951e06fb950
Taylor <zeitue@gmail.com>
2022-03-15 23:30:45 -0500

    portmaster.sh clean up when finished

--- ubuntu/setup/portmaster.sh
+++ ubuntu/setup/portmaster.sh
@@ -3,4 +3,4 @@
 echo "Installing Portmaster"
 wget -c "https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb"
 sudo apt install -y ./portmaster-installer.deb
-#rm ./portmaster-installer.deb
+rm ./portmaster-installer.deb
