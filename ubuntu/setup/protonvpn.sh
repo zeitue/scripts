3a8b9b9fd1007d8860d144052a5eb28e5c68501f
Taylor <zeitue@gmail.com>
2022-03-22 22:52:29 -0500

    Added tray icon for GNOME in protonvpn.sh

--- ubuntu/setup/protonvpn.sh
+++ ubuntu/setup/protonvpn.sh
@@ -5,12 +5,13 @@ URL=$(curl https://protonvpn.com/support/linux-ubuntu-vpn-setup/ |\
      grep all.deb |\
      sed -n 's:.*href="\(.*\)">.*:\1:p' |\
      cut -d\" -f1)
-FILENAME=$(basename "$URL")
-
-wget -c "$URL"
-sudo apt install "./$FILENAME"
+FILENAME=$(basename $URL)
+wget -c "${URL}"
+sudo apt install "./${FILENAME}"
 sudo apt update
 sudo apt install -y protonvpn
-rm "./$FILENAME"
-
+if [[ -f "/usr/bin/gnome-shell" ]]; then
+  sudo apt install -y gnome-shell-extension-appindicator gir1.2-appindicator3-0.1
+fi
+rm "./${FILENAME}"
 
