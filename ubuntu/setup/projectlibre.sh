70ece2cdf208dd21d949b8a00e4bc3c66f97c5ef
Taylor <zeitue@gmail.com>
2022-04-06 23:17:44 -0500

    Added projectlibre.sh

---
+++ ubuntu/setup/projectlibre.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+getLatest() {
+  PRJ=$1
+  curl --silent https://sourceforge.net/projects/$PRJ/best_release.json | \
+    python3 -c "
+import sys;
+from json import loads as l;
+x = l(sys.stdin.read());
+print(x['platform_releases']['linux']['url'])"
+}
+
+URL=$(getLatest 'projectlibre')
+FILENAME=$(basename ${URL%\?*})
+echo "Installing ProjectLibre"
+# seems the API does not like things so quick
+sleep 3
+wget -c --content-disposition "$URL"
+sudo apt install ./$FILENAME
+rm ./$FILENAME
+
