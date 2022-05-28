f0d9e3bdc75ab1bbd97f67b768d70961313b0501
Taylor <zeitue@gmail.com>
2022-05-27 22:13:57 -0500

    Updated github-cli.sh

--- ubuntu/setup/github-cli.sh
+++ ubuntu/setup/github-cli.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+getLatest() {
+  REPO=$1
+  SUFFIX=$2
+  curl --silent https://api.github.com/repos/$REPO/releases/latest | \
+    python3 -c "
+import sys;
+from json import loads as l;
+x = l(sys.stdin.read());
+print(''.join(s['browser_download_url']
+for s in x['assets'] if s['name'].endswith('$SUFFIX')))"
+}
+
+
 # GitHub CLI
 echo "Installing GitHub CLI"
-wget -qO - "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC99B11DEB97541F0" |\
-    gpg --dearmor |\
-    sudo dd status=none of=/etc/apt/trusted.gpg.d/gh.gpg
-sudo apt-add-repository -y https://cli.github.com/packages
-sudo apt update
-sudo apt install -y gh
+URL=$(getLatest cli/cli amd64.deb)
+FILE=$(basename "$URL")
+wget -c "$URL"
+sudo apt install -y ./$FILE
+rm ./$FILE
+
