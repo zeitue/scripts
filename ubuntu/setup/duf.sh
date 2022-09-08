02314e893bd010230e8a3ef63a240959af9a167b
Taylor Ramirez <zeitue@gmail.com>
2022-09-07 23:22:03 -0500

    Added duf.sh

---
+++ ubuntu/setup/duf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+getLatest() {
+  REPO=$1
+  PREFIX=$2
+  SUFFIX=$3
+  curl --silent https://api.github.com/repos/$REPO/releases/latest | \
+    python3 -c "
+import sys;
+from json import loads as l;
+x = l(sys.stdin.read());
+print(''.join(s['browser_download_url']
+for s in x['assets'] if s['name'].endswith('$SUFFIX') and s['name'].startswith('$PREFIX')))"
+}
+
+echo "Installing buf command"
+URL=$(getLatest muesli/duf duf amd64.deb)
+FILENAME=$(basename "$URL")
+wget -c "$URL"
+sudo apt install -y "./$FILENAME"
+rm "$FILENAME"
+
