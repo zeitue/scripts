3137486ccac83681c8a292c3edbec39b553f217b
Taylor Ramirez <zeitue@gmail.com>
2022-09-07 23:07:27 -0500

    Added delta.sh

---
+++ ubuntu/setup/delta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+getLatest() {
+  REPO=$1
+  PREFIX=$2
+  SUFFIX=$3
+  curl --silent https://api.github.com/repos/$REPO/releases/latest | \
+    python3 -c "
+import sys;
+from json import loads as l;
+x = l(sys.stdin.read());
+print(''.join(s['browser_download_url']
+for s in x['assets'] if s['name'].startswith('$PREFIX') and s['name'].endswith('$SUFFIX')))"
+}
+
+echo "Installing delta"
+
+URL=$(getLatest dandavison/delta git-delta_ _amd64.deb)
+FILE=$(basename "$URL")
+wget -c "$URL"
+sudo apt install -y ./$FILE
+rm ./$FILE
