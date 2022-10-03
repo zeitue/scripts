2e0df394acd647b87ef959531bfa94b1b7cefdd9
Taylor Ramirez <zeitue@gmail.com>
2022-10-03 03:35:48 -0500

    Added microsoft-defender.sh

---
+++ linux/ubuntu/setup/microsoft-defender.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing Microsoft Defender"
+VERSION_ID=$(awk -F= '$1 == "VERSION_ID" {gsub(/"/, "", $2); print $2}' /etc/os-release)
+
+curl -o microsoft.list "https://packages.microsoft.com/config/ubuntu/${VERSION_ID}/prod.list"
+sudo mv microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
+
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc |\
+gpg --dearmor |\
+sudo dd status=none of=/etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo apt update
+sudo apt install -y mdatp
