ae2a693a20f9bd8de4a35cde272f5b93064dd54a
Taylor Ramirez <zeitue@gmail.com>
2022-10-14 18:33:26 -0500

    Added extras to pico-sdk.sh

--- linux/ubuntu/setup/pico-sdk.sh
+++ linux/ubuntu/setup/pico-sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install Pico-SDK"
-sudo apt install -y build-essential git cmake
+sudo apt install -y build-essential git cmake gcc-arm-none-eabi minicom thonny
 sudo git clone https://github.com/raspberrypi/pico-sdk.git /opt/pico-sdk
 sudo git -C /opt/pico-sdk submodule update --init
 echo 'export PICO_SDK_PATH=/opt/pico-sdk' |\
